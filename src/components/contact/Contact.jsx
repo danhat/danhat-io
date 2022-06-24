bf8a683c1875e578d7e816a6ea1806a0ce10ea51
danhat <daniellehatten6@gmail.com>
2022-06-24 18:20:31 -0500

    contact section changes

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react'
 import './contact.css'
 // import React, { useRef } from 'react'
 import emailjs from 'emailjs-com'
-import emailData from '../../email.json'
+// import emailData from '../../email.json'
 
 
 const Contact = () => {
@@ -11,7 +11,9 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm(emailData.SERVICE_ID, emailData.TEMPLATE_ID, form.current, emailData.PUBLIC_KEY);
+    // emailjs.sendForm(emailData.SERVICE_ID, emailData.TEMPLATE_ID, form.current, emailData.PUBLIC_KEY);
+
+    emailjs.sendForm(REACT_APP_SERVICE_ID, REACT_APP_TEMPLATE_ID, form.current, REACT_APP_PUBLIC_KEY);
 
     alert('Message sent.')
     e.target.reset();
