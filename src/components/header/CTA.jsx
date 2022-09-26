72f6a9597253e14ffec1db72aa225a6928882e5f
danhat <daniellehatten6@gmail.com>
2022-09-26 18:40:52 -0500

    getting portfolio, home, and about section info from api

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import CV from '../../assets/cv.pdf'
 
+import {useQuery, gql} from '@apollo/client'
+
+
+const GET_INFO = gql`
+  query GetInfo {
+    infos {
+      cv {
+        filename
+        url
+      }
+    }
+  }
+`
+
 const CTA = () => {
+  const {loading, error, data} = useQuery(GET_INFO, {
+    onCompleted: someData =>
+    console.log(someData)
+  })
+  if (loading) return 'Loading...'
+  if (error) return `\n${error}`
+
+  const temp = data.infos.slice()
+  const info = temp[0]
+
+
   return (
     <div className="cta">
-      <a href={CV} download className='btn'>Download CV</a>
+      
+      <a href={info.cv.url} className='btn' target='_blank'>Download CV</a>
       <a href="#contact" className='btn btn-primary'>Message Me</a>
     </div>
   )
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,41 @@ import CTA from './CTA'
 import ME from '../../assets/headshot.png'
 import Nav from '../nav/Nav'
 
+import {useQuery, gql} from '@apollo/client'
+import {AdvancedImage} from '@cloudinary/react'
+import {Cloudinary} from '@cloudinary/url-gen'
+
+
+const GET_INFO = gql`
+  query GetInfo {
+    infos {
+      intro
+      headshot {
+        filename
+      }
+    }
+  }
+`
+
 const Header = () => {
+  const {loading, error, data} = useQuery(GET_INFO, {
+    onCompleted: someData =>
+    console.log(someData)
+  })
+  if (loading) return 'Loading...'
+  if (error) return `\n${error}`
+
+  const temp = data.infos.slice()
+  const info = temp[0]
+
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: process.env.REACT_APP_DANHAT_CLOUDNAME
+    }
+  })
+
+  const myImage = cld.image(`${process.env.REACT_APP_CLD_FOLDER}/${info.headshot.filename}`)
+
   return (
 
     <header className='bg-light'>
@@ -14,15 +48,16 @@ const Header = () => {
       <div className="container header__container" id="home">
         
           <div className="about_headshot">
-          <div className="me">
-            <img src={ME} alt="me"/>
-          </div>
+            <div className="me">
+              <AdvancedImage cldImg={myImage}/>
+            </div>
+          
           </div>
 
           <div className="about_details">
           <h4>Hello, I'm</h4>
           <h1>Danielle Hatten</h1>
-          <h5 className="text-light">Web Developer<br></br>Greater Chicago Area</h5>
+          <h5 className="text-light">{info.intro}</h5>
           <CTA/>
           </div>
         
